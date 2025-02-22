606692cd7dc1fc119a5d453af10ce07c28216ba8
Matthew McQuaid <matthew.mcquaid@gmail.com>
2025-02-22 16:29:20 -0500

    exercise 4.1

---
+++ chapter-4/tutorial-4.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "crypto/sha256"
+
+
+var pc [256]byte
+
+func init() {
+    for i := range pc {
+        pc[i] = pc[i/2] + byte(i&1)
+    }
+}
+
+func popcount(c1, c2 [32]byte) int {
+    var count int
+    for i := 0; i < 32; i++ {
+        count += int(pc[c1[i]^c2[i]])
+    }
+    return count
+}
+
+func main() {
+    c1 := sha256.Sum256([]byte("x"))
+    c2 := sha256.Sum256([]byte("X"))
+    fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
+    fmt.Println("exercise 4.1 - count:", popcount(c1, c2))
+}
