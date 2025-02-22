5d1085e982ab045553bfaef5f669ab4e693205e5
Matthew McQuaid <matthew.mcquaid@gmail.com>
2025-02-22 18:03:45 -0500

    exercise 4.2

--- chapter-4/tutorial-4.go
+++ chapter-4/tutorial-4.go
@@ -20,9 +20,28 @@ func popcount(c1, c2 [32]byte) int {
     return count
 }
 
+func reverse(s []int){
+    for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+        s[i], s[j] = s[j], s[i]
+    }
+}
+
 func main() {
     c1 := sha256.Sum256([]byte("x"))
     c2 := sha256.Sum256([]byte("X"))
     fmt.Printf("%x\n%x\n%t\n%T\n", c1, c2, c1 == c2, c1)
     fmt.Println("exercise 4.1 - count:", popcount(c1, c2))
+
+    // rotate slice
+    s := []int{0, 1, 2, 3, 4, 5}
+    fmt.Println("exercise 4.2:", s)
+    fmt.Println(" s[:2]:", s[:2])
+    reverse(s[:2])
+    fmt.Println(" s[2:]:", s[2:])
+    reverse(s[2:])
+    reverse(s)
+    fmt.Println("exercise 4.2 - reverse:", s)
+    
+
+
 }
